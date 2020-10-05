f4e9f5b3f7af40efcb1842fb037da36178276c5c
mpizarro <maximilianopi@buenosaires.gob.ar>
2020-10-05 17:44:20 -0300

    abms de sigunlar a plurar

--- app.js
+++ app.js
@@ -47,32 +47,32 @@ app.delete('/bajaInscripcionExamen',examenes.bajaInscripcionExamen)
 
 
 // ABM Carreras
-app.post("/carrera", carrera.create);
-app.get("/carrera", carrera.findAll);
-app.get("/carrera/:id", carrera.findOne);
-app.put("/carrera/:id", carrera.update);
-app.delete("/carrera/:id", carrera.delete);
+app.post("/carreras", carrera.create);
+app.get("/carreras", carrera.findAll);
+app.get("/carreras/:id", carrera.findOne);
+app.put("/carreras/:id", carrera.update);
+app.delete("/carreras/:id", carrera.delete);
 
 // ABM Materias
-app.post("/materia", materia.create);
-app.get("/materia", materia.findAll);
-app.get("/materia/:id", materia.findOne);
-app.put("/materia/:id", materia.update);
-app.delete("/materia/:id", materia.delete);
+app.post("/materias", materia.create);
+app.get("/materias", materia.findAll);
+app.get("/materias/:id", materia.findOne);
+app.put("/materias/:id", materia.update);
+app.delete("/materias/:id", materia.delete);
 
 // ABM examen
-app.post("/examen", examen.create);
-app.get("/examen", examen.findAll);
-app.get("/examen/:id", examen.findOne);
-app.put("/examen/:id", examen.update);
-app.delete("/examen/:id", examen.delete);
+app.post("/examenes", examen.create);
+app.get("/examenes", examen.findAll);
+app.get("/examenes/:id", examen.findOne);
+app.put("/examenes/:id", examen.update);
+app.delete("/examenes/:id", examen.delete);
 
 // ABM horario
-app.post("/horario", horario.create);
-app.get("/horario", horario.findAll);
-app.get("/horario/:id", horario.findOne);
-app.put("/horario/:id", horario.update);
-app.delete("/horario/:id", horario.delete);
+app.post("/horarios", horario.create);
+app.get("/horarios", horario.findAll);
+app.get("/horarios/:id", horario.findOne);
+app.put("/horarios/:id", horario.update);
+app.delete("/horarios/:id", horario.delete);
 
 app.listen(port, host);
 console.log(`Running on http://${host}:${port}/`);
